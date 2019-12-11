f522b8fec1c89df52376db5ce786a55f42b6a62c
Nick Rode <theniceguynick@gmail.com>
2019-12-10 21:37:00 -0800

    Accordion, Icons, JS added

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+$("#accordionCustom").on("shown.bs.collapse", e => {
+    $("html, body").animate(
+      {
+        scrollTop: $(e.target)
+          .prev()
+          .offset().top
+      },
+      400
+    );
+  });
