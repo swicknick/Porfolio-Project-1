57627344cea3a1bfdeccb2207fc312ecaf9106e1
Nick Rode <theniceguynick@gmail.com>
2019-12-11 09:50:24 -0800

    Added Articles and Styling

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,18 @@
-$("#accordionCustom").on("shown.bs.collapse", e => {
-    $("html, body").animate(
-      {
-        scrollTop: $(e.target)
-          .prev()
-          .offset().top
-      },
-      400
-    );
-  });
+// $("#accordionCustom").on("hide.bs.collapse show.bs.collapse", e => {
+//   $(e.target)
+//     .prev()
+//     .find("i:last-child")
+//     .toggleClass("fa-minus fa-plus");
+// });
+
+
+// $("#accordionCustom").on("shown.bs.collapse", e => {
+//     $("html, body").animate(
+//       {
+//         scrollTop: $(e.target)
+//           .prev()
+//           .offset().top
+//       },
+//       400
+//     );
+//   });
