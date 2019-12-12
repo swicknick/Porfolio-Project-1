2794e162526848ea7e1d2486ab8ffa1df12a4028
Nick Rode <theniceguynick@gmail.com>
2019-12-11 20:23:04 -0800

    Stuck the Landing

--- myscripts.js
+++ myscripts.js
@@ -1,18 +1,1 @@
-// $("#accordionCustom").on("hide.bs.collapse show.bs.collapse", e => {
-//   $(e.target)
-//     .prev()
-//     .find("i:last-child")
-//     .toggleClass("fa-minus fa-plus");
-// });
 
-
-// $("#accordionCustom").on("shown.bs.collapse", e => {
-//     $("html, body").animate(
-//       {
-//         scrollTop: $(e.target)
-//           .prev()
-//           .offset().top
-//       },
-//       400
-//     );
-//   });
